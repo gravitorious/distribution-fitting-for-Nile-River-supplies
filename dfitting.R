dcf1f5403935cd9b7ee99a18dd6a19d80bf23e33
gravitorious <gravitorious98@gmail.com>
2022-05-29 21:38:24 +0300

    Calculate empirical lmoments

--- dfitting.R
+++ dfitting.R
@@ -4,6 +4,8 @@
 library(xts)
 library(lmom)
 library(lubridate)
+library(moments)
+library(lmom)
 
 # Load functions ----
 source("get_month.R")
@@ -22,4 +24,26 @@ maindata = alldata[paste(startday, endDay, sep = "/")]
 #layout(matrix(c(1,2), 1, 2, byrow = TRUE))
 #hist(maindata)
 #plot(maindata)
+
+# Calculate mean, standard deviation and skewness for each month ----
+means = c()
+stds = c()
+skwns = c()
+
+for(i in 1:12){
+  sample = get_month(maindata, i, 1)
+  means[i] = mean(sample)
+  stds[i] = sd(sample)
+  skwns[i] = moments::skewness(sample)
+}
+
+# Calculate empirical moments ----
+lmoments = list()
+ratiomom = list()
+
+for(i in 1:12){
+  sample = get_month(maindata, i, 1)
+  lmoments[[i]] = lmom::samlmu(x = sample, nmom = 3, ratios = F) #calculate L1, L2, L3
+  ratiomom[[i]] = c(lmoments[[i]][2]/lmoments[[i]][1], lmoments[[i]][3]/lmoments[[i]][2]) #calculate the L-variation and L-skewness via L2/L2 and L3/L2
+}
 
