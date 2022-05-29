5bd73b5247a36c06892580144a174fd29a182f37
gravitorious <gravitorious98@gmail.com>
2022-05-29 22:47:59 +0300

    fit gamma distribution and plot the results

---
+++ EmpCdf.R
@@ -0,0 +1,17 @@
+EmpCdf = function(data, i, plot=1) {
+  
+  cdf = ppoints(n = data, a = 0)
+  xx = sort(data)
+  
+  dfcdf=data.frame(u = cdf, x = xx)
+  
+  if (plot==1) {
+    plot(xx, cdf, type='p', main=paste('Empirical CDF for month:', i), 
+         xlab='x', ylab='Prob.', pch = 19, xlim = c(0, 28) )
+  }
+  
+  return(dfcdf)
+} 
+
+
+
--- dfitting.R
+++ dfitting.R
@@ -8,7 +8,9 @@ library(moments)
 library(lmom)
 
 # Load functions ----
+# we assume that functions are in the same folder with the current file
 source("get_month.R")
+source("EmpCdf.R")
 
 # Load time series and create the appropriate xts object----
 fileName = "NileData_BCM.txt"
@@ -20,8 +22,6 @@ colnames(alldata) = "data"
 startday = "1871-01-01"; endDay = "1944-12-01"
 maindata = alldata[paste(startday, endDay, sep = "/")]
 
-# Plot the time series ----
-#layout(matrix(c(1,2), 1, 2, byrow = TRUE))
 #hist(maindata)
 #plot(maindata)
 
@@ -46,4 +46,23 @@ for(i in 1:12){
   lmoments[[i]] = lmom::samlmu(x = sample, nmom = 3, ratios = F) #calculate L1, L2, L3
   ratiomom[[i]] = c(lmoments[[i]][2]/lmoments[[i]][1], lmoments[[i]][3]/lmoments[[i]][2]) #calculate the L-variation and L-skewness via L2/L2 and L3/L2
 }
+
+# Fit the gamma distribution to the data ----
+temp = seq(from = 0, to = 28, by = 0.0001)
+gamdis = list()
+thgam = list()
+for(i in 1:12){
+  gamdis[[i]] = lmom::pelgam(lmom = lmoments[[i]]) #L3 = 0
+  thgam[[i]] = lmom::cdfgam(x = temp, para = gamdis[[i]])
+}
+
+# Plot empirical and theoretical CDF of the time series by each month ----
+layout_matrix <- matrix(1:6, ncol = 3) 
+layout(layout_matrix)
+for(i in 7:12){
+  sample = get_month(maindata, i, 1)
+  EmpCdf(sample, i)
+  lines(temp, thgam[[i]], col='magenta', lwd =3)
+}
+
 
