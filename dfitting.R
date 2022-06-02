1282c8394679078fa3ee38a7e061eb4978aeb59c
gravitorious <gravitorious98@gmail.com>
2022-06-02 23:52:35 +0300

    fit more distributions using fitdistrplus library and compare the results

--- dfitting.R
+++ dfitting.R
@@ -6,6 +6,7 @@ library(lmom)
 library(lubridate)
 library(moments)
 library(lmom)
+library(fitdistrplus)
 
 # Load functions ----
 # we assume that functions are in the same folder with the current file
@@ -67,3 +68,43 @@ for(i in 7:12){
 
 #create a time series expressing the mean Nile River supplies for each year ----
 ymaindata = xts::apply.yearly(x = maindata, mean)
+
+
+# Fit the Weibull distribution to the data
+temp2 = seq(from = 0, to = 28, by = 0.0001)
+weidis = list()
+thwei = list()
+for(i in 1:12){
+  weidis[[i]] = lmom::pelwei(lmom = lmoments[[i]]) #we pass the first 3 moments, but L3 is not used
+  thwei[[i]] = lmom::cdfwei(x = temp2, para = weidis[[i]])
+}
+layout_matrix2 <- matrix(1:6, ncol = 3) 
+layout(layout_matrix2)
+for(i in 7:12){
+  sample2 = get_month(maindata, i, 1)
+  EmpCdf(sample2, i)
+  lines(temp, thwei[[i]], col='blue', lwd =3)
+}
+
+# Fit the lognormal, weibull and gamma distributions for month 9 with fitdistrplus library
+sample3 = get_month(maindata, 9, 1)
+fitGamma= fitdist(data = sample3, distr = 'gamma', method = 'mle') #gamma
+
+fitWei = fitdist(data = sample3, distr = 'weibull', method = 'mle') #weibull
+
+fitLnorm = fitdist(data = sample3, distr = 'lnorm', method = 'mle') #lognormal
+
+Fits=list()
+Fits[[1]] = fitGamma
+Fits[[2]] = fitWei
+Fits[[3]] = fitLnorm
+fitdistrplus::cdfcomp(ft = Fits)
+
+# print the Akaike information criterion (AIC) value for each distribution
+#AIC = 2k – 2(Log-Likelihood) where k is the number of parameters.
+print(paste("The AIC for Gamma distribution is:", fitGamma$aic))
+print(paste("The AIC for Weibull distribution is:", fitWei$aic))
+print(paste("The AIC for LogNormal distribution is:", fitLnorm$aic))
+summary(fitGamma)
+summary(fitWei)
+summary(fitLnorm)
